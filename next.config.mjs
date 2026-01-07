a0bbbfb74ecfc071617c445e5aaaf19113b16911
Rahul Kushwaha <rk.rahulkushwaha22@gmail.com>
2026-01-07 18:11:38 +0530

    Re-add basePath for gh-pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/viratsbuildtech',
+  assetPrefix: '/viratsbuildtech',
   typescript: {
     ignoreBuildErrors: true,
   },

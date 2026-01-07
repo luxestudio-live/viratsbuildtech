2d71e66e105783d1fb9db79a85aea4a35ecf0f3d
Rahul Kushwaha <rk.rahulkushwaha22@gmail.com>
2026-01-07 18:08:07 +0530

    Switch to gh-pages branch deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/viratsbuildtech',
-  assetPrefix: '/viratsbuildtech/',
   typescript: {
     ignoreBuildErrors: true,
   },

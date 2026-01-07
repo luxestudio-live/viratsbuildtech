441f0372712b0c781603eaadadb5cdf0ac8ecfc0
Rahul Kushwaha <rk.rahulkushwaha22@gmail.com>
2026-01-07 17:52:27 +0530

    Fix basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
+  basePath: '/viratsbuildtech',
   typescript: {
     ignoreBuildErrors: true,
   },
